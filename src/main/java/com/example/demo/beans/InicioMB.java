c810c8721c76115124b7990d9f5a1407c83850c6
cchipana <cchipana@grupobisa.com>
2020-11-27 10:22:47 -0400

    cambio proxy3

--- src/main/java/com/example/demo/beans/InicioMB.java
+++ src/main/java/com/example/demo/beans/InicioMB.java
@@ -18,7 +18,7 @@ public class InicioMB implements Serializable {
 
     Logger logger = LoggerFactory.getLogger(InicioMB.class);
 
-    private String holaMundo = "Inicial otro saludo cambio en proxy !";
+    private String holaMundo = "Inicial otro saludo cambio en proxy3 !";
 
     @PostConstruct
     public void init() {
